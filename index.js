cb6719bd99e81407a26a69cdc68a82a8bc0f964d
Mamtu Raja <mamturaja89@gmail.com>
2021-12-28 18:46:53 +0530

    adding keyboard listener

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-
+//detecting buttonpressed on the screen
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
   document.querySelectorAll("button")[i].addEventListener("click",function() {
-    if(this.innerHTML=='w'){
+    var buttonChar=this.innerHTML;
+    makeSound(buttonChar);
+  });
+}
+
+//detecting keyboardPress
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+});
+
+function makeSound(key){
+  switch (key) {
+    case 'w':
       var audio=new Audio('sounds/crash.mp3');
       audio.play();
-    }
-    if(this.innerHTML=='a'){
+      break;
+    case 'a':
       var audio=new Audio('sounds/kick-bass.mp3');
       audio.play();
-    }
-    if(this.innerHTML=='s'){
+      break;
+    case 's':
       var audio=new Audio('sounds/snare.mp3');
       audio.play();
-    }
-    if(this.innerHTML=='d'){
+      break;
+    case 'd':
       var audio=new Audio('sounds/tom-1.mp3');
       audio.play();
-    }
-    if(this.innerHTML=='j'){
+      break;
+   case 'j':
       var audio=new Audio('sounds/tom-2.mp3');
       audio.play();
-    }
-    if(this.innerHTML=='k'){
+      break;
+   case 'k':
       var audio=new Audio('sounds/tom-3.mp3');
       audio.play();
-    }
-    if(this.innerHTML=='l'){
+      break;
+   case 'l':
       var audio=new Audio('sounds/tom-4.mp3');
       audio.play();
-    }
-  });
+      break;
 
+    default:
+    console.log(key);
+
+  }
 }
